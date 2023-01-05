b8c61b60108c6e13e7cf6bb5e9e57691bcc11bef
Jiaxu Liu <34015028+ljxw88@users.noreply.github.com>
2023-01-05 15:11:13 +0000

    update

--- run_pandemic.sh
+++ run_pandemic.sh
@@ -1,4 +1,4 @@
 rm ./submission/submission.zip
 SUBMISSION_TRACK=pandemic make pack-submission
-# SUBMISSION_TRACK=pandemic SUBMISSION_TYPE=federated make test-submission
-SUBMISSION_TRACK=pandemic SUBMISSION_TYPE=centralized make test-submission
+SUBMISSION_TRACK=pandemic SUBMISSION_TYPE=federated make test-submission
+# SUBMISSION_TRACK=pandemic SUBMISSION_TYPE=centralized make test-submission
